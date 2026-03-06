b1af4ea100ebe2255b458512e3f570ce54285e33
AndrasHacks <mr.acs.andras@gmail.com>
2026-03-06 11:49:49 +0100

    some more common js syntax

--- node-js-crash/index.js
+++ node-js-crash/index.js
@@ -1,4 +1,8 @@
-const generateRandomNumber = require("./utils");
+const { generateRandomNumber, celsiusToFahrenheit } = require("./utils");
 
 console.log(process.env.LANGUAGE);
-console.log(generateRandomNumber());
+console.log(`Random number: ${generateRandomNumber()}`);
+const celsius = generateRandomNumber();
+console.log(
+  `Celsius to fahrenheit: ${celsius}C is ${celsiusToFahrenheit(celsius)}F`
+);
--- node-js-crash/utils.js
+++ node-js-crash/utils.js
@@ -2,4 +2,11 @@ function generateRandomNumber() {
   return Math.floor(Math.random() * 100 + 1);
 }
 
-module.exports = generateRandomNumber;
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+module.exports = {
+  generateRandomNumber,
+  celsiusToFahrenheit,
+};
