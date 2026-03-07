13e01aec30728ab0282412103c4d434c8363422c
AndrasHacks <mr.acs.andras@gmail.com>
2026-03-07 08:34:15 +0100

    http server returning json

--- node-js-crash/server.js
+++ node-js-crash/server.js
@@ -1,8 +1,8 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello World!</h1>");
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "server error" }));
 });
 
 const PORT = 3000;
