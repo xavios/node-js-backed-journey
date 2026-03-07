2d57ee58bd929c8fbfa89748c5afa47828cdb8c5
AndrasHacks <mr.acs.andras@gmail.com>
2026-03-07 09:13:05 +0100

    basic routing

--- node-js-crash/server.js
+++ node-js-crash/server.js
@@ -1,16 +1,42 @@
 import http from "http";
 const PORT = process.env.PORT;
 
+const recipeDetailsUrlRegex =
+  /^\/recipes\/..\-..\/details\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/gm;
+
 const server = http.createServer((req, res) => {
-  res.writeHead(500, { "Content-Type": "text/html" });
-  res.end(`
-    <html>
-        <body>
-            <center>
-                <h1>Hello from the Node.js environment</h1>
-            </center>
-        </body>
-    </html>`);
+  const url = req.url;
+  const method = req.method;
+  if (method !== "GET") {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "server error" }));
+    return;
+  }
+
+  switch (url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`<h1>Recipes Home Page</h1>`);
+      break;
+
+    case "/recipes":
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`<h1>Recipes HUB Page</h1>`);
+      break;
+
+    default:
+      if (url.match(recipeDetailsUrlRegex)) {
+        const recipeUUIDIdx = url.lastIndexOf("/");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(
+          `<h1>Recipes Details Page</h1><h2>${url.substring(recipeUUIDIdx + 1)}</h2>`
+        );
+        return;
+      }
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.end(`not found`);
+      break;
+  }
 });
 
 server.listen(PORT, () => {
